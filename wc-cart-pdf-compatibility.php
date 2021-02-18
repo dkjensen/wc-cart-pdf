bd76eb52a5c86e9a1e4c07f155ba3f2fd5cc1799
David Jensen <david@dkjensen.com>
2021-02-18 13:28:27 -0800

    fix: add compatibility with gravity pdf

--- wc-cart-pdf-compatibility.php
+++ wc-cart-pdf-compatibility.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin compatibility
- * 
+ *
  * @package wc-cart-pdf
  */
 
@@ -13,10 +13,21 @@
  * @return void
  */
 function wc_cart_pdf_compatibility_tm_extra_product_options() {
-    add_filter( 'wc_epo_no_edit_options', '__return_true' ); // Hide "Edit options" link on product title
+	add_filter( 'wc_epo_no_edit_options', '__return_true' ); // Hide "Edit options" link on product title
 }
 add_action( 'wc_cart_pdf_before_process', 'wc_cart_pdf_compatibility_tm_extra_product_options' );
 
+/**
+ * Gravity PDF
+ *
+ * @return void
+ */
+function wc_cart_pdf_compatibility_gravity_pdf() {
+	if ( class_exists( 'GFPDF_Major_Compatibility_Checks' ) && isset( $GLOBALS['gravitypdf'] ) && isset( $_GET['cart-pdf'] ) ) {
+		remove_action( 'plugins_loaded', array( $GLOBALS['gravitypdf'], 'plugins_loaded' ) );
+	}
+}
+add_action( 'plugins_loaded', 'wc_cart_pdf_compatibility_gravity_pdf', 0 );
 
 /**
  * Visual Products Configurator
@@ -24,12 +35,15 @@ add_action( 'wc_cart_pdf_before_process', 'wc_cart_pdf_compatibility_tm_extra_pr
  * @return void
  */
 function wc_cart_pdf_compatibility_visual_products_configurator() {
-    add_filter( 'vpc_get_config_data', function( $thumbnail_code ) {
-        $edit_i18n = __( 'Edit', 'vpc' );
-        
-        $thumbnail_code = preg_replace( '/<\s*a[^>]*>' . $edit_i18n . '<\s*\/\s*a>/', '', $thumbnail_code ); // Hide the "Edit" link
+	add_filter(
+		'vpc_get_config_data',
+		function( $thumbnail_code ) {
+			$edit_i18n = __( 'Edit', 'vpc' );
 
-        return $thumbnail_code;
-    } );
+			$thumbnail_code = preg_replace( '/<\s*a[^>]*>' . $edit_i18n . '<\s*\/\s*a>/', '', $thumbnail_code ); // Hide the "Edit" link
+
+			return $thumbnail_code;
+		}
+	);
 }
 add_action( 'wc_cart_pdf_before_process', 'wc_cart_pdf_compatibility_visual_products_configurator' );
