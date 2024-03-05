069f62d95b4dfab6638eb4f0378a31f6c26f2b05
David Jensen <david@dkjensen.com>
2024-03-05 17:34:25 -0500

    feat: wc blocks checkout compatibility

--- resources/js/wc-cart-pdf.js
+++ resources/js/wc-cart-pdf.js
@@ -1,24 +1,74 @@
 import Cookies from 'js-cookie';
+import { debounce } from 'lodash';
 
-(function ($) {
-    const $form = $('form[name="checkout"]');
+(function () {
+	const captureFields = cartpdf.capture_fields || [];
 
-    const captureFields = cartpdf.capture_fields || [];
+	const saveCustomerData = ( object ) => {
+		try {
+			if ( !object || typeof object !== 'object') {
+				throw new Error('Invalid object');
+			}
 
-    $form.on('change', ':input', function (e) {
-        e.preventDefault();
+			// Filter out object keys that are not in the captureFields array.
+			for (const key in object) {
+				if ( !captureFields.includes( key ) ) {
+					delete object[key];
+				}
+			}
 
-        let formData = new FormData(document.querySelectorAll('form[name="checkout"]')[0]);
+			const json = JSON.stringify( object );
 
-        var object = {};
-        formData.forEach(function(value, key) {
-            if (captureFields.includes(key)) {
-                object[ key ] = value;
-            }
-        });
+			Cookies.set('wc-cart-pdf-customer', json);
+		} catch (error) {
+			console.error( error );
+		}
+	};
 
-        var json = JSON.stringify(object);
+	/**
+	 * WooCommerce Blocks Checkout
+	 */
+	if ( typeof wp !== 'undefined' && wp && wp.data ) {
+		const debouncedSubscription = debounce(() => {
+			const select = wp.data.select('wc/store/cart');
+			const customerData = select.getCustomerData();
 
-        Cookies.set('wc-cart-pdf-customer', json);
-    });
-})(jQuery);
+			const object = {};
+
+			for (const key in customerData.shippingAddress) {
+				object[`shipping_${key}`] = customerData.shippingAddress[key];
+			}
+
+			for (const key in customerData.billingAddress) {
+				object[`billing_${key}`] = customerData.billingAddress[key];
+			}
+
+			saveCustomerData( object );
+		}, 250);
+
+		wp.data.subscribe( debouncedSubscription );
+	}
+
+	/**
+	 * Legacy Checkout
+	 */
+	const form = document.querySelector('form[name="checkout"]');
+	const formElements = form?.querySelectorAll( 'input, textarea, select' );
+
+	formElements?.forEach((element) => {
+		element.addEventListener('change', function (e) {
+			e.preventDefault();
+
+			const formData = new FormData(form);
+
+			console.log( formData );
+
+			const object = {};
+			formData.forEach(function (value, key) {
+				object[key] = value;
+			});
+
+			saveCustomerData( object );
+		});
+	});
+})();
